f5794def8eda50036e3cff13124a317b0d0de178
Germanna Rebouças <ger_manna@hotmail.com>
2022-08-24 21:47:51 -0300

    Create POO_03_Professor.java

---
+++ POO_03_Professor.java
@@ -0,0 +1,17 @@
+
+package POO.Exemplo02;
+
+
+public class Aula02Professor03 {
+    //Atributos
+    public Aula02Disciplina03 disciplina;
+    public String nomeProfessor;
+    public int idProfessor;
+    
+    //Método construtor
+    public Aula02Professor03(String nomeProfessor, int idProfessor){
+        this.nomeProfessor = nomeProfessor;
+        this.idProfessor = idProfessor;
+    }
+    
+}
