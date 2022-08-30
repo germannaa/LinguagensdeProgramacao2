4b6d43a1e768686cfbd6cd1e8fe1defa521efc1c
Germanna Rebouças <ger_manna@hotmail.com>
2022-08-29 21:11:11 -0300

    Update POO_04_Disciplina.java

--- POO_04_Disciplina.java
+++ POO_04_Disciplina.java
@@ -3,16 +3,18 @@ package POO.Exemplo03;
 
 
 public class Disciplina {
+    public Professor professor;
     public String nomeDisciplina;
     public int idDisciplina;
     
     //Método construtor
-    public Disciplina(String nomeDisciplina, int idDisciplina){
+    public Disciplina(String nomeDisciplina, int idDisciplina, Professor professor){
         this.nomeDisciplina = nomeDisciplina;
         this.idDisciplina = idDisciplina;
+        this.professor = professor;
     }
     
-    public String getNomedisciplina(){
+    public String getNomeDisciplina(){
         return nomeDisciplina;
     }
     
