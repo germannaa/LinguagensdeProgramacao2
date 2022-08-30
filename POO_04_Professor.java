4c56d52b6c19d57520b99269d18d8f0cd0d86506
Germanna Rebouças <ger_manna@hotmail.com>
2022-08-29 21:10:19 -0300

    Update POO_04_Professor.java

--- POO_04_Professor.java
+++ POO_04_Professor.java
@@ -1,4 +1,5 @@
 
+
 package POO.Exemplo03;
 
 import java.util.ArrayList;
@@ -10,12 +11,14 @@ public class Professor {
     public String nomeProfessor;
     public int idProfessor;
     
+    //Lista de disciplinas que o professor pode ter
     public List<Disciplina> disciplinas;
     
     //Método construtor
     public Professor(String nomeProfessor, int idProfessor){
         this.nomeProfessor = nomeProfessor;
         this.idProfessor = idProfessor;
+        //Instanciando a lista, com o objeto c o array vazio
         disciplinas = new ArrayList<>();
     }
     
@@ -25,6 +28,20 @@ public class Professor {
     
     public String getNomeProfessor(){
         return nomeProfessor;
+    }
+    
+    public void addDisciplina(Disciplina disciplina){
+        this.disciplinas.add(disciplina);
+    }
+    
+    public void getTodasDisciplinas(){
+         System.out.println("Disciplinas do professor:");
+        //Um ponteiro
+        //Tipo de dado do array, o C é uma variavel de acesso (ponteiro de fato), depois a lista que vai percorrer
+        //Um array dinamico, disperso na memoria.
+        for(Disciplina c: disciplinas){
+           System.out.println(c.nomeDisciplina);
+        }
     }
     
     
