b26b8948b6bb00296c250bf34c09481a5bf478bb
Germanna Rebouças <ger_manna@hotmail.com>
2022-08-29 21:51:21 -0300

    Update POO_05_Disciplinas.java

--- POO_05_Disciplinas.java
+++ POO_05_Disciplinas.java
@@ -1,1 +1,42 @@
 
+
+package POO.Exemplo04;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+
+public class Disciplina {
+    public List<Professor> professores;
+    public String nomeDisciplina;
+    public int idDisciplina;
+    
+    //Método construtor
+    public Disciplina(String nomeDisciplina, int idDisciplina, Professor professor){
+        this.nomeDisciplina = nomeDisciplina;
+        this.idDisciplina = idDisciplina;
+        professores = new ArrayList<>();
+    }
+    
+    public String getNomeDisciplina(){
+        return nomeDisciplina;
+    }
+    
+    public int getIdDisciplina (){
+        return idDisciplina;
+    }
+    
+    public void addProfessor(Professor professor){
+        this.professores.add(professor);
+    }
+    
+    public void mostrarProfessor(){
+        for (Professor p: professores){
+            System.out.println(p.getNomeProfessor());
+        }
+    }
+
+    
+    }
+
