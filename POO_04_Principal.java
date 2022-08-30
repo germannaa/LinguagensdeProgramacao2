a4e550d3620c148607a01626217f11bbeff2c515
Germanna Rebouças <ger_manna@hotmail.com>
2022-08-29 21:10:44 -0300

    Update POO_04_Principal.java

--- POO_04_Principal.java
+++ POO_04_Principal.java
@@ -2,5 +2,17 @@ package POO.Exemplo03;
 
 
 public class Principal {
+    public static void main(String[]args){
+        Professor mauricio = new Professor("Mauricio neto", 001);
+        Professor fulano = new Professor ("Fulano", 002);
+        Disciplina poo = new Disciplina("Programacao orientada a objeto", 123, mauricio);
+        Disciplina lp1 = new Disciplina("LP1", 123, mauricio);
+
+        mauricio.addDisciplina(poo);
+        mauricio.addDisciplina(lp1);
+
+        mauricio.getTodasDisciplinas();
+
+    }
     
 }
