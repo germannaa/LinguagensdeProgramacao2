16c59f4d0b05ec4b011eb106e2f07bbf338352f4
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-05 21:50:28 -0300

    Create SuperClasse.java

---
+++ POO/POO_11/SuperClasse.java
@@ -0,0 +1,13 @@
+
+package POO.Exemplo09;
+
+public class SuperClasse {
+     public int atributo1;
+     private int atributo2;
+     protected int  atributo3;
+        
+    public SuperClasse(){
+       
+    }
+    
+}
