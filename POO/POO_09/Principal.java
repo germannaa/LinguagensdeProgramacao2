0937ba0d2125671f5fdaada71525c7e4eede389d
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-05 21:21:53 -0300

    Update Principal.java

--- POO/POO_09/Principal.java
+++ POO/POO_09/Principal.java
@@ -1,1 +1,13 @@
-.
+package POO.Exemplo08;
+
+
+public class Principal {
+    public static void main(String[]args){
+        Subclasse subclasse = new Subclasse();
+        
+        subclasse.metodo1();
+        subclasse.metodo2();
+        subclasse.metodo3();
+    }
+    
+}
