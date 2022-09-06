308e7912c5638f644e4ab043518bd0472e66ba76
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-05 21:12:36 -0300

    Update Principal.java

--- POO/POO_08/Principal.java
+++ POO/POO_08/Principal.java
@@ -1,1 +1,25 @@
-.
+
+package POO.Exemplo07;
+
+public class Calculadora {
+ 
+        public double sum(double a, double b){
+            return (double) a+b;
+        }
+        
+        public int sum(int... args){
+            int total = 0;
+            for(int i:args){
+                total+=i;
+            }
+            return total;
+        }
+    
+    
+   public static void main(String[]args){
+       Calculadora calc = new Calculadora();
+       System.out.println(calc.sum(5.5, 10.7));
+       System.out.println(calc.sum(1,2,3,4));
+   }
+    
+}
