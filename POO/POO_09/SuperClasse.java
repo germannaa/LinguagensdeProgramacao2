e40934fb17e91b0a57dbc186437917145749e6c3
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-05 21:22:28 -0300

    Create SuperClasse.java

---
+++ POO/POO_09/SuperClasse.java
@@ -0,0 +1,17 @@
+
+package POO.Exemplo08;
+
+
+public class SuperClasse {
+    public SuperClasse(){
+        
+    }
+    
+    public void metodo1(){
+        System.out.println("Chamando metodo 1 da SUPERCLASSE");
+    }
+    
+    public void metodo2(){
+        System.out.println("Chamando metodo 2 da SUPERCLASSE");
+    }
+}
