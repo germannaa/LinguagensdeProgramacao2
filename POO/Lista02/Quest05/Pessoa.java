c8fa712f9debafc990640a3e1ebd290fd0d42020
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-12 21:40:27 -0300

    Create Pessoa.java

---
+++ POO/Lista02/Quest05/Pessoa.java
@@ -0,0 +1,22 @@
+
+package Lista2_Ex05;
+
+
+public class Pessoa {
+    private int codigo;
+    protected String nome;
+    
+    public Pessoa(int codigo){
+        this.codigo = codigo;
+    }
+    
+    public int getCodigo(){
+        return codigo;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    
+}
