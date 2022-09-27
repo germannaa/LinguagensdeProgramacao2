a8b62cef65a153e1955b1c05efadb6c133c9b3e2
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-26 21:49:46 -0300

    Create Imovel.java

---
+++ Polimorfismo/Exemplo02/Imovel.java
@@ -0,0 +1,24 @@
+package Polimorfismo02;
+
+
+public class Imovel {
+    private Double preco;
+    protected String endereco;
+
+    public Imovel(Double preco) {
+        this.preco = preco;
+    }
+
+
+    public Double getPreco() {
+        return preco;
+    }
+    
+    public Double Propriedade(Double preco){
+        return preco;
+    }
+    
+    public Double getPrecoImovel(){
+        return preco;
+    }
+}
