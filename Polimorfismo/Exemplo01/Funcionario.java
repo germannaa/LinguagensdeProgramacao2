7ecb85870ce008f578af3c7aa890f46557d69507
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-26 21:45:49 -0300

    Create Funcionario.java

---
+++ Polimorfismo/Exemplo01/Funcionario.java
@@ -0,0 +1,24 @@
+package Polimorfismo01;
+
+
+public class Funcionario extends Pessoa {
+    private double salario;
+
+    public Funcionario(String nome) {
+        super(nome);
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+    
+    @Override
+    public void imprimir() {
+        System.out.println("Funcionario: " + getNome());
+    }
+    
+}
