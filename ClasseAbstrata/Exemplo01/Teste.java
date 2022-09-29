bface123adb17b2476cdc9fb807f26ab317b679c
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-28 21:24:45 -0300

    Create Teste.java

---
+++ ClasseAbstrata/Exemplo01/Teste.java
@@ -0,0 +1,13 @@
+package Abstrata01;
+
+
+public class Teste {
+    public static void main (String[] args){
+    Product heman = new Toy("Mestre do Universo", 500);
+    Product bola = new Ball("Bola Quadrada", 1500);
+
+heman.showInfo();   
+bola.showInfo();
+    
+}
+}
