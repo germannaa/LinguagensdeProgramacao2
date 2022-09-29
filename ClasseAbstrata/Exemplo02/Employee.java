64278e126688605769eec0b45a15e1accafc8778
Germanna Rebouças <ger_manna@hotmail.com>
2022-09-28 21:31:47 -0300

    Create Employee.java

---
+++ ClasseAbstrata/Exemplo02/Employee.java
@@ -0,0 +1,29 @@
+
+public abstract class Employee {
+    public String name;
+    public double salary;
+
+    public Employee() {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    public abstract void increaseSalary();
+    
+}
