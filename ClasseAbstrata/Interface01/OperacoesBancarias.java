0059206f2b947afd38ad5f044c0522d43c3a9cca
Germanna Rebouças <ger_manna@hotmail.com>
2022-10-03 21:22:17 -0300

    Create OperacoesBancarias.java

---
+++ ClasseAbstrata/Interface01/OperacoesBancarias.java
@@ -0,0 +1,12 @@
+
+package Interface01;
+
+//INTERFACE
+
+public interface OperacoesBancarias {
+    public void Sacar(double valor);       
+    public void Depositar(double valor);
+    public double Saldo();
+    
+    //APENAS NA CLASSE PAI!!!!!!
+}
