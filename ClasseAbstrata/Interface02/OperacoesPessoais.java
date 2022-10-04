4be8f82a9db6e21483b271f8c76e33acf0fe5ce3
Germanna Rebouças <ger_manna@hotmail.com>
2022-10-03 21:36:41 -0300

    Create OperacoesPessoais.java

---
+++ ClasseAbstrata/Interface02/OperacoesPessoais.java
@@ -0,0 +1,9 @@
+package Interface02;
+
+
+public interface OperacoesPessoais {
+    public void getNome(String nome);
+    public String setNome(String nome);
+    public void getId(int id);
+    public int setId(int id);
+}
