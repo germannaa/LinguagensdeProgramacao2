136f183dbd4523f7cfb0615f40f9d02d5cc1e861
Germanna Rebouças <reboucasgermanna@gmail.com>
2022-10-19 21:36:05 -0300

    Create Teste.java

---
+++ AtributoEstatico/Exercicio01/Teste.java
@@ -0,0 +1,13 @@
+package AtributoEstatico01;
+
+
+public class Teste {
+    public static void main(String[] args) {        
+        
+        Usuario usuario1 = new Usuario();
+        
+        Usuario usuario2 = new Usuario();
+        
+    }
+   
+}
