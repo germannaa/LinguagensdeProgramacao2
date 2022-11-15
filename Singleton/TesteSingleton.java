1d12c17a363bf7db7195834b4ad518162777181f
Germanna Rebouças <reboucasgermanna@gmail.com>
2022-11-14 21:20:57 -0300

    Create TesteSingleton.java

---
+++ Singleton/TesteSingleton.java
@@ -0,0 +1,11 @@
+
+package Singleton;
+
+public class testeSingleton {
+        public static void main(String[] args){
+            Singleton singleton = Singleton.getInstance();
+            
+            singleton.testInstance();
+        }
+    }
+
